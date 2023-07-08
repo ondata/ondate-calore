38583cc2d6c6deac21de7af320977175c13813f2
aborruso <aborruso@gmail.com>
2023-07-08 10:36:43 +0200

    update

--- ondate-calore.sh
+++ ondate-calore.sh
@@ -25,13 +25,13 @@ else
     curl $url -o $folder/processing/output.html
 fi
 
-# estrai header
+# estrai header, e converti data in formato ISO
 <$folder/processing/output.html scrape -be '//table/thead' | xq -r '.html.body.thead.tr.th[]."#text"' | paste -sd, | sed -E 's#([0-9]{2})/([0-9]{2})/([0-9]{4})#\3-\2-\1#g' >$folder/processing/ondate-calore.csv
 
 # estrai dati
 <$folder/processing/output.html scrape -be '//tr[@class="grigio-05-bg"]' | xq -c '.html.body.tr[]|{name:.td[0].span.a."@name",prima:.td[1].img."@alt",seconda:.td[2].img."@alt",terza:.td[3].img."@alt"}'  | mlr --j2c cat | tail -n +2 >>$folder/processing/ondate-calore.csv
 
-# trasforma da wide a long
+# trasforma struttura dati da wide a long
 mlr --csv label citta then reshape -r "[0-9]" -o data,livello then sort -r data  -f citta then put '$data_estrazione="'"$data"'"' $folder/processing/ondate-calore.csv >$folder/data/ondate-calore_latest.csv
 
 # se il file di archivio non esiste, crea un file vuoto
