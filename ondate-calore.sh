84b4a92f6be61f7e10b029f5a986bacd1dbbc8b1
aborruso <aborruso@gmail.com>
2023-07-09 14:37:37 +0200

    aggiunto info livelli

--- ondate-calore.sh
+++ ondate-calore.sh
@@ -55,6 +55,8 @@ fi
 mlr --c2n cut -f data then uniq -a "$folder"/data/ondate-calore_latest.csv | while read -r line; do
     if [[ $line == *"$data"* ]]; then
         mlr --csv join --ul -j citta -f "$folder"/data/ondate-calore_latest.csv then unsparsify then filter '$data=="'"$data"'"' "$folder"/data/citta-anagrafica.csv >"$folder"/data/ondate-calore_oggi.csv
+        mlr --csv join --ul -j livello -f "$folder"/data/ondate-calore_oggi.csv then unsparsify "$folder"/risorse/livelli.csv >"$folder"/processing/tmp.csv
+        mv "$folder"/processing/tmp.csv "$folder"/data/ondate-calore_oggi.csv
     else
         echo "non contiene la stringa $data"
     fi
